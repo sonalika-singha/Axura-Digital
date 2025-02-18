93cc9eeebbb0fbd8b42e37c0200c01da42101fc6
hevardhan <paatukkorupulavan@gmail.com>
2025-02-18 15:56:55 +0530

    initial commit with some minor changes

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import  "./css/Navbar.css"
+
+const Navbar = () => {
+  return (
+    <div className='navbar bg-[#f0fdff]'>
+        <h1 className='bg-[#010893]'>Axura Digital</h1>
+    </div>
+  )
+}
+
+export default Navbar
