d837d211af75f826ded55adc4f0c8343db8af4e3
hevardhan <paatukkorupulavan@gmail.com>
2025-02-22 11:43:48 +0530

    home page commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,22 @@ import  "./css/Navbar.css"
 
 const Navbar = () => {
   return (
-    <div className='navbar bg-[#f0fdff]'>
-        <h1 className='bg-[#010893]'>Axura Digital</h1>
+    <div className='navbar flex justify-between px-3'>
+      <div className="logo flex text-center items-center">
+        <div className="logo-img items-center flex justify-center">
+          <img src="" alt="Logo" />
+        </div>
+        <div className="logo-text ms-3 text-xl">
+          <p>AXURA DIGITAL</p>
+        </div>
+      </div>
+      <div className="nav-items items-center flex">
+        <ul className='flex gap-4 items-center'>
+          <li className='px-4 py-2'>About</li>
+          <li className='px-4 py-2'>Service</li>
+          <li className='px-4 py-2'>Contact</li>
+        </ul>
+      </div>
     </div>
   )
 }
